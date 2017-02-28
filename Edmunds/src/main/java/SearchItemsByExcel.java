ca807b424fc8e3c9784f1a4937ab26ee71070307
Alex Gladyshev <aleksey.gladyshev@gmail.com>
2017-02-28 15:35:23 -0500

    changing xpaths

---
+++ Edmunds/src/main/java/SearchItemsByExcel.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by aleks_000 on 2/28/2017.
+ */
+public class SearchItemsByExcel {
+
+    @Test
+    public void search() throws IOException, InterruptedException{
+
+
+    }
+}
--- Edmunds/src/test/java/TestHomepageLinks.java
+++ Edmunds/src/test/java/TestHomepageLinks.java
@@ -1,13 +1,21 @@
+import base.CommonAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 /**
  * Created by aleks_000 on 2/20/2017.
  */
-public class TestHomepageLinks {
+public class TestHomepageLinks extends CommonAPI {
 
     @Test
-    public void TestLinks(){
+    public void TestMakeButton(){
 
+        driver.findElement(By.xpath(".//option[@value='honda']")).click();
+        driver.findElement(By.xpath(".//option[@value='civic'][contains(text(), 'Civic Sedan')]")).click();
+        driver.findElement(By.xpath(".//option[@value = '2015']")).click();
+        driver.findElement(By.xpath(".//*[@id='edm-entry-home-page-hero-carousel']/div/div[12]/div/div[1]/div[4]/div/button[1]")).
+                click();
 
     }
 }
